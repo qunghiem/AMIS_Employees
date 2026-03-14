7d5caa88fa6e8f1728241f4445a05335f1f5f098
Quân Nghiêm <nghiemxuanquan2003@gmail.com>
2026-03-14 15:39:28 +0700

    fix UI

--- js/main.js
+++ js/main.js
@@ -5,10 +5,22 @@ document.addEventListener("DOMContentLoaded", () => {
   // Toggle sidebar thu gọn / mở rộng
   const toggleBtn = document.querySelector(".sidebar__toggle");
   const sidebar = document.querySelector(".layout__sidebar");
-
-  // bắt sự kiện click thu gọn - mở rộng sidebar
+  const sidebarToggle = document.querySelector(".sidebar__toggle");
+ 
+  // bắt sự kiện clickthu gọn - mở rộng sidebar
+  let checkToggle = false;
   toggleBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("collapsed");
+    if (!checkToggle) {
+      sidebar.classList.add("collapsed");
+      checkToggle = true;
+      toggleBtn.style.justifyContent = "center";
+      sidebarToggle.style.width = "70%";
+    } else {
+      sidebar.classList.remove("collapsed");
+      checkToggle = false;
+      toggleBtn.style.justifyContent = "flex-start";
+      sidebarToggle.style.width = "87%";
+    }
   });
 
   // chạy trang ứng viên
